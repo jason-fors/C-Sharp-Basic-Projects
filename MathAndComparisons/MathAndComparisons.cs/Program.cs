b3d542aa9bce9ae7a013a97f9b05151eb3033558
Jason-fors <jasonfors@hotmail.com>
2021-04-11 14:34:49 -0400

    Add MathandComparisons
    Compares annual salaries of 2 people based on user input.

---
+++ MathAndComparisons/MathAndComparisons.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MathAndComparisons.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1 Hourly Rate: ");
+            double hourlyPerson1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Person 1 Hours Worked per Week: ");
+            double hoursWorkedPerson1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Person 2 Hourly Rate: ");
+            double hourlyPerson2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Person 2 Hours Worked per Week: ");
+            double hoursWorkedPerson2 = Convert.ToDouble(Console.ReadLine());
+
+            double annualSalaryPerson1 = hourlyPerson1 * Convert.ToDouble(52) * hoursWorkedPerson1;
+            Console.WriteLine("Annual salary of Person 1: $" + annualSalaryPerson1);
+            double annualSalaryPerson2 = hourlyPerson2 * Convert.ToDouble(52) * hoursWorkedPerson2;
+            Console.WriteLine("Annual salary of Person 2: $" + annualSalaryPerson2);
+
+            bool personOneMakesMore = annualSalaryPerson1 > annualSalaryPerson2;
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(personOneMakesMore);
+
+            Console.ReadLine();
+        }
+    }
+}
