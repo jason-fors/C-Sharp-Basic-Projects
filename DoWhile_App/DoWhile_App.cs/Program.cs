e4f4ee3885ca701ff937905aa730e338dd758110
Jason-fors <jasonfors@hotmail.com>
2021-04-27 10:17:47 -0400

    Add Do-While App

---
+++ DoWhile_App/DoWhile_App.cs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DoWhile_App.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepPlaying = true;
+
+            while (keepPlaying)
+            {
+                // Gets a number from the user
+                Console.WriteLine("Pick a number for me to count to.");
+                int userNum = Convert.ToInt32(Console.ReadLine());
+
+                // Starts count at 1
+                int i = 1;
+
+                // Counts to the users number. With the do-while loop, will include 1 if the user picks a number less than 1.
+                do
+                {
+                    Console.WriteLine("Count: " + i.ToString());
+                    i++;
+
+                } while (i <= userNum);
+
+
+                // Asks user if they want to play again.
+                Console.WriteLine("Type 'y' to play again.");
+                string playAgain = Console.ReadLine();
+
+                if (playAgain == "y")
+                {
+                    keepPlaying = true;
+                }
+                else
+                {
+                    keepPlaying = false;
+                }
+            }
+        }
+    }
+}
