8bd49ec8764c1515361fa872496d73237a033e09
Jason-fors <jasonfors@hotmail.com>
2021-04-29 16:23:15 -0400

    Add ClassAndMethod2

---
+++ ClassAndMethod2/ClassAndMethod2.cs/MathOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndMethod2.cs
+{
+    // Creates a class for math operations
+    public class MathOperations
+    {
+
+        // Takes two integers as parameters and returns the product
+        public int multiply(int num1, int num2=2)
+        {
+            return num1 * num2;
+        }
+        
+    }
+}
---
+++ ClassAndMethod2/ClassAndMethod2.cs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassAndMethod2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Instantiating MathOperations object so we can use its methods.
+            MathOperations mathOperations1 = new MathOperations();
+
+            // Obtaining numbers from the user to perform math operations on.
+            Console.WriteLine("Pick an integer to multiply.");
+            int userNumber1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Pick another integer to multiply (optional). If you don't pick a number, your number will be multiplied by 2.");
+            
+            string userResponse2 = Console.ReadLine();
+
+            int product;  // Declaring product variable here rather than as local variable in if/else.
+
+            if (userResponse2 == "")
+            {
+                product = mathOperations1.multiply(userNumber1);
+            }
+            else
+            {
+                int userNumber2 = Convert.ToInt32(userResponse2);
+                product = mathOperations1.multiply(userNumber1, userNumber2);
+            }
+            
+            Console.WriteLine("The product is: " + product);
+
+            Console.ReadLine();
+        }
+    }
+}
