06dd3f952d7c07ec9790734c1899f9a73678b78e
Jason-fors <jasonfors@hotmail.com>
2021-04-29 10:26:46 -0400

    Add ClassAndMethod app

---
+++ ClassAndMethod/ClassAndMethod.cs/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndMethod.cs
+{
+    // Creates a class for math operations
+    public class MathOperations
+    {
+
+        // Takes an integer argument and returns the square
+        public int Square(int userNum)  
+        {
+            return userNum * userNum;
+        }
+
+        // Takes an integer argument and returns its sum with 4
+        public int PlusFour(int userNum)
+        {
+            return userNum + 4;
+        }
+
+        // Takes an integer argument and returns its product with 5
+        public int TimesFive(int userNum)
+        {
+            return userNum * 5;
+        }
+    }
+}
---
+++ ClassAndMethod/ClassAndMethod.cs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassAndMethod.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Obtaining an number from the user to perform math operations on.
+            Console.WriteLine("Pick a number to perform some math operations on!");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Instantiating MathOperations object so we can use its methods.
+            MathOperations mathOperations = new MathOperations();
+
+            // Calling each MathOperations method on the user number and assigning each result to a varible.
+            int squared = mathOperations.Square(userNumber);
+            int plusFour = mathOperations.PlusFour(userNumber);
+            int timesFive = mathOperations.TimesFive(userNumber);
+
+            // Displaying results to user on console.
+            Console.WriteLine("Your number squared is: " + squared);
+            Console.WriteLine("Your number plus four is: " + plusFour);
+            Console.WriteLine("Your number times five is: " + timesFive);
+
+
+            Console.ReadLine();
+        }
+    }
+}
