543e4b8daca9c3eb3582c8956e580d366365471c
Jason-fors <jasonfors@hotmail.com>
2021-04-29 17:40:05 -0400

    Add VoidMethod

---
+++ VoidMethod/VoidMethod/MathOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethod
+{
+    class MathOperations
+    {
+     
+        public void doStuff(int num1, int num2)
+        {
+            int num3 = num1 * num1;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOperation = new MathOperations();
+            mathOperation.doStuff(num1: 6, num2: 5);            
+        }
+    }
+}
