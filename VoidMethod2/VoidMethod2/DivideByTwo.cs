a82a2e46f29b0ef1fca07e9c0e76bf53d27cc85f
Jason-fors <jasonfors@hotmail.com>
2021-04-29 18:26:13 -0400

    Add VoidMethod2
    Includes use of out parameter, overloaded method and static method.

---
+++ VoidMethod2/VoidMethod2/DivideByTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethod2
+{
+    public class DivideByTwo
+    {
+        public static int divisor = 2;
+
+        // A method handing result back using an output parameter.
+        public void Divide2(int num1, out int result)
+        {
+            result = num1 / divisor;
+        }
+
+        // Overloading method...this one just presents result on console.
+        public void Divide2(int num1)
+        {
+            Console.WriteLine(num1 / divisor);
+        }
+
+        // Static method to change divisor for the class.
+        public static void setDivisor(int newDivisor)
+        {
+            divisor = newDivisor;
+        }
+    }
+}
---
+++ VoidMethod2/VoidMethod2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VoidMethod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate class
+            DivideByTwo divideByTwo = new DivideByTwo();
+
+            // Get number from user and call divide2 method from class, displaying output parameter to console
+            Console.WriteLine("Enter a number.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            divideByTwo.Divide2(userNumber, out int result1);
+            Console.WriteLine(result1);
+
+            // Change number with static method and calling overloaded method (no out parameter, method displays result to console).
+            DivideByTwo.setDivisor(3);
+            divideByTwo.Divide2(userNumber);
+        }
+    }
+}
