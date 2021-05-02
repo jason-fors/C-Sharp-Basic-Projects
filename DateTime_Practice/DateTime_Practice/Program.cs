92125e8f895a5075aae05dcf22bc5bc26c230cf2
Jason-fors <jasonfors@hotmail.com>
2021-05-02 15:30:50 -0400

    Add DateTime_Practice

---
+++ DateTime_Practice/DateTime_Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DateTime_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display current time to console.
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime);
+
+            // Get a number from user and add it to current datetime.
+            Console.WriteLine("Please give me a number.");
+            double hours = Convert.ToDouble(Console.ReadLine());
+            DateTime newTime = currentTime.AddHours(hours);
+
+            // Display new datetime in console.
+            Console.WriteLine(newTime);
+
+
+        }
+    }
+}
