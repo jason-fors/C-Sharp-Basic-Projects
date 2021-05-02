6940e5bf928b93f56e0d6c0e4894c09e7dba5a9e
Jason-fors <jasonfors@hotmail.com>
2021-05-02 14:59:30 -0400

    Add IO_Practice

---
+++ IO_Practice/IO_Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace IO_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string text = Console.ReadLine();
+            // This overwrites the contents of the file.
+            File.WriteAllText(@"C:\Users\Jason Fors\Desktop\IO_Practice\Logs\log.txt", text);
+
+            string textRead = File.ReadAllText(@"C:\Users\Jason Fors\Desktop\IO_Practice\Logs\log.txt");
+            Console.WriteLine(textRead);
+
+        }
+    }
+}
---
+++ TwentyOne/TwentyOne.cs/TwentyOneDealer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOne.cs
+{
+    public class TwentyOneDealer: Dealer
+    {
+        // Need to make sure the lists is not empty. Cannot add to null, so starting with empty list.
+        private List<Card> _hand = new List<Card>();
+        public List<Card> Hand { get { return _hand; } set { _hand = value; } }
+
+        public bool Stay { get; set; }
+        public bool isBusted { get; set; }
+    }
+}
