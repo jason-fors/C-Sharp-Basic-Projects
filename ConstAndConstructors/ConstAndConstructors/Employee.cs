8390be47aa3ca1fc09b8d3dbff5e56daa830e3e2
Jason-fors <jasonfors@hotmail.com>
2021-05-03 08:17:11 -0400

    Add ConstAndConstructors
    Practice with constructors and constants

---
+++ ConstAndConstructors/ConstAndConstructors/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstAndConstructors
+{
+    public class Employee
+    {
+        public Employee(): this(1, "Staff")
+        {
+
+        }
+        
+        public Employee(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        public string Title { get; set; }
+        public int Id { get; set; }
+
+
+    }
+}
---
+++ ConstAndConstructors/ConstAndConstructors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConstAndConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string employer = "Acme, Inc.";
+
+            Employee employee1 = new Employee();
+
+            Console.WriteLine(employee1.Id);
+            Console.WriteLine(employee1.Title);
+            Console.WriteLine(employer);
+
+        }
+    }
+}
