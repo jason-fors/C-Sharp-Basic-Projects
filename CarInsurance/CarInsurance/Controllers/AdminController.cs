6d75e048789d6e5b8a1f99e36b2a1d6e4fd8d2b3
Jason-fors <jasonfors@hotmail.com>
2021-05-06 19:25:06 -0400

    Add Car Insurance App
    Visual Studio .Net Web Application

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,86 @@
+﻿using CarInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace CarInsurance.Views.Admin
+{
+    public class AdminController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Admin
+        public ActionResult Index(int? id)
+        {
+
+            //IList<Client> clientList = from a in _dbFeed.Client.Include("Auto") select a
+
+            //// Make list of insurees -- Entity won't let you save to db inside a foreach loop.
+            //List<Insuree> insurees = select * from db.insurees;
+
+            var insurees = db.Insurees.Where(x => x.Id != null).ToList();
+
+            foreach (var insuree in insurees)
+            {
+
+                decimal startQuote = 50m;
+                TimeSpan age = DateTime.Now - insuree.DateOfBirth;
+                double ageYears = age.Days / 365.25;
+                if (ageYears < 19)
+                {
+                    startQuote += 100;
+                }
+                else if (ageYears < 26)
+                {
+                    startQuote += 50;
+                }
+                else
+                {
+                    startQuote += 25;
+                }
+
+                if (insuree.CarYear < 2000 || insuree.CarYear > 2015)
+                {
+                    startQuote += 25;
+                }
+
+                if (insuree.CarMake == "Porche")
+                {
+                    startQuote += 25;
+
+                    if (insuree.CarModel == "Carerra")
+                    {
+                        startQuote += 25;
+                    }
+                }
+
+                startQuote += (insuree.SpeedingTickets * 10);
+
+                if (insuree.DUI)
+                {
+                    startQuote *= (decimal)1.25;
+                }
+
+                if (insuree.CoverageType)
+                {
+                    startQuote *= (decimal)1.5;
+                }
+
+                insuree.Quote = startQuote;
+
+                if (ModelState.IsValid)
+                {
+                    db.SaveChanges();
+                }
+
+            }
+            return View(db.Insurees.ToList());
+        }
+
+    }
+}
---
+++ CarInsurance/CarInsurance/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Let's face it, insurance is a commodity. Find out if ours is the most affordable.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Hopefully you won't be needing this contact information!";
+
+            return View();
+        }
+    }
+}
