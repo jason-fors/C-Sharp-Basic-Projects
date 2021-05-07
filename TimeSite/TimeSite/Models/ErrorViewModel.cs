6954e6bea938150e4ec5c8f6c24612fd0b0977ba
Jason-fors <jasonfors@hotmail.com>
2021-05-07 15:13:13 -0400

    Add TimeSite
    Simple .NET Core web app that shows the current time.

---
+++ TimeSite/TimeSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TimeSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
